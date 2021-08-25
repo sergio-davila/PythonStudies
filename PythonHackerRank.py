7082333b164ffe2c859dae151ae1bf1562712dab
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-24 21:54:45 -0500

    feat: solved python if else

--- PythonHackerRank.py
+++ PythonHackerRank.py
@@ -10,3 +10,16 @@ if n % 2 == 1 | ((n >= 6 & n <= 20) & n % 2 == 0):
     print("Weird")
 else:
     print("Not Weird")
+
+# second better solution
+if __name__ == '__main__':
+    n = int(input().strip())
+if n % 2 != 0:
+    print ("Weird")
+else:
+    if n >= 2 and n <= 5:
+        print("Not Weird")
+    elif n >= 6 and n <= 20:
+        print("Weird")
+    elif n > 20:
+        print("Not Weird")
