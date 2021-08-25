6035929981925f35c0ca4c4e7cd5e9fd41844c15
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-24 21:43:05 -0500

    feat: first commit added gitignore

---
+++ PythonHackerRank.py
@@ -0,0 +1,12 @@
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if n % 2 == 1 | ((n >= 6 & n <= 20) & n % 2 == 0):
+    print("Weird")
+else:
+    print("Not Weird")
