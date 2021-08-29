059e508d997ae3783c505148ceb079ffce0d876f
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-08-29 02:50:31 -0500

    feat: solved finding the percentage problem in python

--- PythonHackerRank.py
+++ PythonHackerRank.py
@@ -119,3 +119,22 @@ if __name__ == '__main__':
     second_students = sorted([s for s, g in students if g == second_lowest])
     for s in second_students:
         print(s)
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    for key, value in student_marks.items():
+        if query_name == key:
+            sum = 0
+            count = 0
+            for i in value:
+                sum += i
+                count += 1
+            average = sum/count
+            print("{:.2f}".format(average))
